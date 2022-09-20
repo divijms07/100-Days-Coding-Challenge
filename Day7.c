d0cf7abf3d3857529ac2ec9ad8821c9838e353d9
Divij Sarkale <109907102+divijms07@users.noreply.github.com>
2022-09-21 00:40:04 +0530

    Linked List Insert Part

---
+++ Day7.c
@@ -0,0 +1,85 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+	int info;
+	struct node *next;
+};
+
+struct node *list = NULL;
+
+struct node* getnode()
+{
+	return ((struct node*)malloc(sizeof(struct node)));
+}
+
+void freenode(struct node *p)
+{
+	free(p);
+}
+
+void insertbeg(int x)
+{
+	struct node *nn;
+	nn = getnode();
+	nn->info = x;
+	nn->next = list;
+	list = nn;
+}
+
+void insertend(int x)
+{
+	struct node *nn, *temp;
+	nn = getnode();
+	nn->info = x;
+	nn->next = NULL;
+	temp = list;
+	while(temp->next!=NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = nn;
+}
+
+void insert(int pos, int val)
+{
+	
+	if(list == NULL)
+	{
+		char ch;
+		printf("List is Empty!\n");
+		printf("Do you want to add element at first position ? ");
+		scanf("%c", &ch);
+		if(ch == 'y' || ch == 'Y')
+		{
+			insertbeg(val);
+		}
+		else
+		{
+			return;
+		}
+	}
+	else
+	{
+		int count = 1;
+		struct node *nn, *t;
+		t = list;
+		
+		while(t->next!=NULL)
+		{
+
+			if(count == pos-1)
+			{
+				nn = getnode();
+				nn->info = val;
+				nn->next = t->next;
+				t->next = nn;
+				break;
+			}
+			t=t->next;
+			count++;
+		}
+	}
+
+}
