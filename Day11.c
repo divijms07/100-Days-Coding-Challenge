b0b39556c2b38a9e6b1f3e7b223bbf3d97b65c58
Divij Sarkale <109907102+divijms07@users.noreply.github.com>
2022-09-25 00:32:34 +0530

    Code Chef Cakewalk Program

---
+++ Day11.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+int main(void) 
+{
+	int t;
+	scanf("%d",&t);
+    while (t--){
+	    int x;
+	    scanf("%d",&x);
+	    if(x<7){
+	        printf("YES\n");
+	    }
+        else {
+	    printf("\nNO");
+	    }
+	}
+	return 0;
+}
+
